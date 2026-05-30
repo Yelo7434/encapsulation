dc9b0691326854b1ebdf3cb50a524f9351671033
unknown <hernandezrichtericefrancisco@gmail.com>
2026-05-30 11:35:13 +0800

    first fan and second

--- the_fan_class/test_fan.py
+++ the_fan_class/test_fan.py
@@ -12,3 +12,10 @@ fan_2.set_speed(Fan.MEDIUM)
 fan_2.set_radius(5)
 fan_2.set_color("blue")
 fan_2.set_on(False)
+
+# Display fan information
+print("FIRST FAN")
+print(fan_1)
+
+print("SECOND FAN")
+print(fan_2)
