337746a351dd042502c6ae570d41e9b7825f1275
unknown <hernandezrichtericefrancisco@gmail.com>
2026-05-30 11:47:16 +0800

    fix error

--- the_fan_class/fan.py
+++ the_fan_class/fan.py
@@ -4,41 +4,41 @@ class Fan:
     MEDIUM = 2
     FAST = 3
 
-# Construction
-def __init__(self, speed=1, radius=5.0, color="blue", on=False):
+    # Construction
+    def __init__(self, speed=1, radius=5.0, color="blue", on=False):
         self.__speed = speed
         self.__radius = radius
         self.__color = color
         self.__on = on
 
-# Getters
-def get_speed(self):
+    # Getters
+    def get_speed(self):
         return self.__speed
 
-def get_radius(self):
+    def get_radius(self):
         return self.__radius
 
-def get_color(self):
+    def get_color(self):
         return self.__color
 
-def is_on(self):
+    def is_on(self):
         return self.__on
 
-# Setters
-def set_speed(self, speed):
+    # Setters
+    def set_speed(self, speed):
         self.__speed = speed
 
-def set_radius(self, radius):
+    def set_radius(self, radius):
         self.__radius = radius
 
-def set_color(self, color):
+    def set_color(self, color):
         self.__color = color
 
-def set_on(self, on):
+    def set_on(self, on):
         self.__on = on
 
-# String representation
-def __str__(self):
+    # String representation
+    def __str__(self):
         if self.__on:
             return (
                 f"Fan is ON\n"
