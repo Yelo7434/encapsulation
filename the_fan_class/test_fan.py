f51075e30c4882860f2c4681c7ae31c85f5d9ae3
unknown <hernandezrichtericefrancisco@gmail.com>
2026-05-30 11:34:09 +0800

    test fan

---
+++ the_fan_class/test_fan.py
@@ -0,0 +1,14 @@
+from fan import Fan
+# First fan object
+fan_1 = Fan()
+fan_1.set_speed(Fan.FAST)
+fan_1.set_radius(10)
+fan_1.set_color("yellow")
+fan_1.set_on(True)
+
+# Second fan object
+fan_2 = Fan()
+fan_2.set_speed(Fan.MEDIUM)
+fan_2.set_radius(5)
+fan_2.set_color("blue")
+fan_2.set_on(False)
