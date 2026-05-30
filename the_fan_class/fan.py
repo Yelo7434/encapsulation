109a37032e17ee7111361332227b225246cd1e1b
unknown <hernandezrichtericefrancisco@gmail.com>
2026-05-30 11:04:31 +0800

    Setting the getters and setters

--- the_fan_class/fan.py
+++ the_fan_class/fan.py
@@ -4,11 +4,35 @@ class Fan:
     MEDIUM = 2
     FAST = 3
 
-#construction
+# Construction
 def __init__(self, speed=SLOW, radius=5.0, color="blue", on=False):
         self.__speed = speed
         self.__radius = radius
         self.__color = color
         self.__on = on
 
-        
+# Getters
+def get_speed(self):
+        return self.__speed
+
+def get_radius(self):
+        return self.__radius
+
+def get_color(self):
+        return self.__color
+
+def is_on(self):
+        return self.__on
+
+# Setters
+def set_speed(self, speed):
+        self.__speed = speed
+
+def set_radius(self, radius):
+        self.__radius = radius
+
+def set_color(self, color):
+        self.__color = color
+
+def set_on(self, on):
+        self.__on = on
