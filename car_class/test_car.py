1931c54c92279ce233101fb4d14cf5b119c42a5a
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-01 16:41:39 +0800

    print car information

--- car_class/test_car.py
+++ car_class/test_car.py
@@ -1,3 +1,7 @@
 from test_car import Car
 # Create a Car object
 car_1 = Car(2024, "Lamborghini")
+
+print("Car Information")
+print(f"Year Model: {car_1.get_year_model()}")
+print(f"Make: {car_1.get_make()}")
