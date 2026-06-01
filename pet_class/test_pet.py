65a626ba4e4c75563bd495031013ca4810553936
unknown <hernandezrichtericefrancisco@gmail.com>
2026-06-01 17:19:14 +0800

    Display pet information

--- pet_class/test_pet.py
+++ pet_class/test_pet.py
@@ -8,3 +8,8 @@ pet_1.set_name("Yelo")
 pet_1.set_animal_type("Cat")
 pet_1.set_age(1)
 
+# Display pet information
+print("Pet Information")
+print(f"Name: {pet_1.get_name()}")
+print(f"Animal Type: {pet_1.get_animal_type()}")
+print(f"Age: {pet_1.get_age()}")
